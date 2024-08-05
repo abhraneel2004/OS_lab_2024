328facc118a3d72268942ab9dddc89a2bd4b8ec8
Abhraneel Karmakar <115551450+abhraneel2004@users.noreply.github.com>
2024-08-05 14:56:39 +0000

    String_input_check

---
+++ 16_check_string.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Function to check if the input is a string of alphabets
+is_alpha() {
+  
+}
+
+# Read input from command line
+input=$1
+
+# Call the function with the user input
+is_alpha "$input"
