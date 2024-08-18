696ca34928fa8f4f9ed53f4b8fd5f09bec1b4bbe
Abhraneel Karmakar <115551450+abhraneel2004@users.noreply.github.com>
2024-08-18 16:02:21 +0000

    file added

---
+++ file_db/db.sh
@@ -0,0 +1,77 @@
+FILE="db.txt"
+insert(){
+  flag=1
+  read -p "Enter Roll: " roll
+  lines=$(wc -l < $FILE)
+  for((i=2; i<=lines; i++)); do
+    troll=$(head -n $i $FILE| tail -n 1| cut -d "|" -f 2)
+    if [[ $troll == $roll ]]; then
+      flag=0
+      break
+    fi
+  done
+  if [[ $flag -eq 1 ]]; then
+    read -p "Enter Name: " name
+    read -p "Enter Marks: " marks
+    echo "$name|$roll|$marks" >> $FILE
+    echo "Insert Successful"
+  else
+    echo "Data Already Present"
+    echo "Insert Unsuccessful"
+  fi
+  
+}
+
+update(){
+  flag=0
+  read -p "Enter Roll: " roll
+  lines=$(wc -l < $FILE)
+  for((i=1; i<=lines; i++)); do
+    troll=$(head -n $i $FILE| tail -n 1| cut -d "|" -f 2)
+    if [[ $troll == $roll ]]; then
+      read -p "Enter New Name: " nname
+      read -p "Enter New Marks: " nmarks
+      echo "$nname|$troll|$nmarks" >> temp.txt
+      flag=1
+    else
+      name=$(head -n $i $FILE| tail -n 1| cut -d "|" -f 1)
+      marks=$(head -n $i $FILE| tail -n 1| cut -d "|" -f 3)
+      echo "$name|$troll|$marks" >> temp.txt
+    fi
+  done
+  mv temp.txt $FILE
+  if [ $flag -eq 1 ]; then
+    echo "Update Successful"
+  else
+    echo "No data about Roll no.: $roll found"
+  fi
+}
+
+delete(){
+  read -p "Enter the roll to delete: " roll
+  grep -v "|$roll|" $FILE > temp.txt && mv temp.txt $FILE
+}
+
+display(){
+  cat $FILE
+  echo " "
+}
+
+while [ true ]; do
+  printf "\n-----------------------\nOperations\n"
+  echo "1. Insert"
+  echo "2. Update"
+  echo "3. Delete"
+  echo "4. Display"
+  echo "5. Exit"
+  echo "~~~~~~~~~~~~~~~~~"
+  read -p "Enter your choice: " choice
+  case $choice in
+    1) insert ;;
+    2) update ;;
+    3) delete ;;
+    4) display ;;
+    5) exit;;
+    *) echo "Invalid choice" ;;
+  esac
+done
--- gcd_checker/program_checker.sh
+++ gcd_checker/program_checker.sh
@@ -1,15 +1,22 @@
 `ls ./GCD_programs > temp2.txt` # create a temporary file
 num_lines=$(wc -l < temp2.txt)
-
+# echo "number of files: $num_lines"
 for ((i=1; i <= num_lines; i++)) do
 	filename=`head -n $i temp2.txt|tail -n 1`
 	# test.txt contains the test cases
-	l=$(wc -l < test.txt) # no of test cases
+	# l=$(wc -l < test.txt) # no of test cases
+  l=$(cat test.txt | wc -l)
+  echo "no. of test cases $l"
 	p=0
 	for (( j=1 ; j <= $l; j++)) do
 		x=`head -n $j test.txt|tail -n 1|cut -d " " -f 1`
 		y=`head -n $j test.txt|tail -n 1|cut -d " " -f 2`
 		z=`head -n $j test.txt|tail -n 1|cut -d " " -f 3`
+    # echo "+++++++++++++"
+    # echo "x = $x"
+    # echo "y = $y"
+    # echo "z = $z"
+    # echo "+++++++++++++"
 		out=$(sh ./GCD_programs/$filename $x $y)
 	
 		if [ $out == $z ]; then
